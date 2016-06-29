9ea4710a34c553f22d882ae0cc85bd3a02334523
Prelang Builder <builder@prelang.com>
2016-06-29 04:30:31 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Product' to allow for create, update

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Product do
+  permit_params :name, :description, :price
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
